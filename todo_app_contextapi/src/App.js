40d17578882bf7dbef7e08f339c2a9d65ea202d7
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-08-19 23:45:37 +0300

    Update App.js

--- todo_app_contextapi/src/App.js
+++ todo_app_contextapi/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
+const INITIAL_STATE = [ //Proğramın başlandıcında atadığımız varsayılan değer.
+  { id: 1, desc: "Toplantı notlarını unutma", completed: false, isEditing: false },
+  { id: 2, desc: "Müşterileri aramayı unutma", completed: false, isEditing: false },
+  { id: 3, desc: "Spor salonundan randevu iptali", completed: true, isEditing: false },
+  { id: 4, desc: "Proje yönetimi görüşmesi", completed: false, isEditing: false },
+  { id: 5, desc: "Javascript konferansını unutma", completed: false, isEditing: false }
+
+];
+
 function App() {
   return (
     <>
-      Hello
+      <TodoWrapper />
     </>
   );
 }
