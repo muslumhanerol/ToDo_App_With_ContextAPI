017682152ebbd6bd83a6b710a0de7d6737a6712b
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-08-24 23:48:19 +0300

    Update App.js

--- todo_app_contextapi/src/App.js
+++ todo_app_contextapi/src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import TodoWrapper from './Components/TodoWrapper';
 import { useId } from 'react';
 
-const INITIAL_STATE = [ //Proğramın başlandıcında atadığımız varsayılan değer.
-  { id: useId(), desc: "Toplantı notlarını unutma", completed: false, isEditing: false },
-  { id: useId(), desc: "Müşterileri aramayı unutma", completed: false, isEditing: false },
-  { id: useId(), desc: "Spor salonundan randevu iptali", completed: true, isEditing: false },
-  { id: useId(), desc: "Proje yönetimi görüşmesi", completed: false, isEditing: false },
-  { id: useId(), desc: "Javascript konferansını unutma", completed: false, isEditing: false }
-];
+
 
 function App() {
+  const INITIAL_STATE = [ //Proğramın başlandıcında atadığımız varsayılan değer.
+    { id: useId(), desc: "Toplantı notlarını unutma", completed: false, isEditing: false },
+    { id: useId(), desc: "Müşterileri aramayı unutma", completed: false, isEditing: false },
+    { id: useId(), desc: "Spor salonundan randevu iptali", completed: true, isEditing: false },
+    { id: useId(), desc: "Proje yönetimi görüşmesi", completed: false, isEditing: false },
+    { id: useId(), desc: "Javascript konferansını unutma", completed: false, isEditing: false }
+  ];
+
+
   return (
     <>
       <TodoWrapper tasks={INITIAL_STATE} />
