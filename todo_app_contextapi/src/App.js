839b781b178ddf0f92e79dea0b32ed978b90bd5c
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-08-24 23:58:11 +0300

    Update App.js

--- todo_app_contextapi/src/App.js
+++ todo_app_contextapi/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import TodoWrapper from './Components/TodoWrapper';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as idGenerate } from 'uuid';
 
 function App() {
   const INITIAL_STATE = [ //Proğramın başlandıcında atadığımız varsayılan değer.
-    { id: useId(), desc: "Toplantı notlarını unutma", completed: false, isEditing: false },
-    { id: useId(), desc: "Müşterileri aramayı unutma", completed: false, isEditing: false },
-    { id: useId(), desc: "Spor salonundan randevu iptali", completed: true, isEditing: false },
-    { id: useId(), desc: "Proje yönetimi görüşmesi", completed: false, isEditing: false },
-    { id: useId(), desc: "Javascript konferansını unutma", completed: false, isEditing: false }
+    { id: idGenerate(), desc: "Toplantı notlarını unutma", completed: false, isEditing: false },
+    { id: idGenerate(), desc: "Müşterileri aramayı unutma", completed: false, isEditing: false },
+    { id: idGenerate(), desc: "Spor salonundan randevu iptali", completed: true, isEditing: false },
+    { id: idGenerate(), desc: "Proje yönetimi görüşmesi", completed: false, isEditing: false },
+    { id: idGenerate(), desc: "Javascript konferansını unutma", completed: false, isEditing: false }
   ];
 
 
