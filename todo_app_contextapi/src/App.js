d346c1110551121adfde0a1ae09f2d74eba2520c
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-08-24 23:44:44 +0300

    Update App.js

--- todo_app_contextapi/src/App.js
+++ todo_app_contextapi/src/App.js
@@ -4,10 +4,10 @@ import { useId } from 'react';
 
 const INITIAL_STATE = [ //Proğramın başlandıcında atadığımız varsayılan değer.
   { id: useId(), desc: "Toplantı notlarını unutma", completed: false, isEditing: false },
-  { id: 2, desc: "Müşterileri aramayı unutma", completed: false, isEditing: false },
-  { id: 3, desc: "Spor salonundan randevu iptali", completed: true, isEditing: false },
-  { id: 4, desc: "Proje yönetimi görüşmesi", completed: false, isEditing: false },
-  { id: 5, desc: "Javascript konferansını unutma", completed: false, isEditing: false }
+  { id: useId(), desc: "Müşterileri aramayı unutma", completed: false, isEditing: false },
+  { id: useId(), desc: "Spor salonundan randevu iptali", completed: true, isEditing: false },
+  { id: useId(), desc: "Proje yönetimi görüşmesi", completed: false, isEditing: false },
+  { id: useId(), desc: "Javascript konferansını unutma", completed: false, isEditing: false }
 ];
 
 function App() {
