4e4376d5b8e46c23757b95c63ca9fed59abf39d5
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-08-25 00:01:38 +0300

    Update App.js

--- todo_app_contextapi/src/App.js
+++ todo_app_contextapi/src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import TodoWrapper from './Components/TodoWrapper';
 import { v4 as idGenerate } from 'uuid';
 
-function App() {
-  const INITIAL_STATE = [ //Proğramın başlandıcında atadığımız varsayılan değer.
-    { id: idGenerate(), desc: "Toplantı notlarını unutma", completed: false, isEditing: false },
-    { id: idGenerate(), desc: "Müşterileri aramayı unutma", completed: false, isEditing: false },
-    { id: idGenerate(), desc: "Spor salonundan randevu iptali", completed: true, isEditing: false },
-    { id: idGenerate(), desc: "Proje yönetimi görüşmesi", completed: false, isEditing: false },
-    { id: idGenerate(), desc: "Javascript konferansını unutma", completed: false, isEditing: false }
-  ];
+const INITIAL_STATE = [ //Proğramın başlandıcında atadığımız varsayılan değer.
+  { id: idGenerate(), desc: "Toplantı notlarını unutma", completed: false, isEditing: false },
+  { id: idGenerate(), desc: "Müşterileri aramayı unutma", completed: false, isEditing: false },
+  { id: idGenerate(), desc: "Spor salonundan randevu iptali", completed: true, isEditing: false },
+  { id: idGenerate(), desc: "Proje yönetimi görüşmesi", completed: false, isEditing: false },
+  { id: idGenerate(), desc: "Javascript konferansını unutma", completed: false, isEditing: false }
+];
 
+function App() {
 
   return (
     <>
