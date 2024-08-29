402c5da8812c192ca0cd30f3cd84a5d61d93174b
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-08-29 23:32:42 +0300

    Update TodoAddForm.js

--- todo_app_contextapi/src/Components/TodoAddForm.js
+++ todo_app_contextapi/src/Components/TodoAddForm.js
@@ -3,10 +3,11 @@ import React from 'react'
 const TodoAddForm = () => {
     return (
 
-        <div className='container'>
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+        <div className='container w-50'>
+            <h1 className='display-5'>Todo App</h1>
+            <div className="input-group mb-3">
+                <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                <button className="btn btn-outline-success" type="button" id="button-addon2">Task Ekle</button>
             </div>
         </div>
 
