671a2fa9d36ea000a1b89f1bce1a42d4aceffa73
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-08-29 23:46:38 +0300

    Update TodoAddForm.js

--- todo_app_contextapi/src/Components/TodoAddForm.js
+++ todo_app_contextapi/src/Components/TodoAddForm.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
 const TodoAddForm = () => {
+    const handleSubmit = () => {
+
+    }
     return (
 
         <div className='container w-50 p-5'>
             <h1 className='display-5 text-center mb-5 text-primary'>Todo App</h1>
-            <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Görev yaz" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                <button className="btn btn-outline-success" type="button" id="button-addon2">Task Ekle</button>
-            </div>
+            {/* form butonuna tıklandığında handleSubmit fonks. çalıştır. */}
+            <form onSubmit={handleSubmit}>
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Görev yaz" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <button className="btn btn-outline-success" type="submit" id="btn-add-task">Task Ekle</button>
+                </div>
+            </form>
         </div>
 
     )
