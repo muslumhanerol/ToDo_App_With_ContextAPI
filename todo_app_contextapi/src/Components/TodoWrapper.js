9abd1a9de570400b31a53ae5be884fff17231a46
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-08-31 00:06:15 +0300

    Update TodoWrapper.js

--- todo_app_contextapi/src/Components/TodoWrapper.js
+++ todo_app_contextapi/src/Components/TodoWrapper.js
@@ -35,7 +35,7 @@ const TodoWrapper = ({ tasks }) => {
         );
     }
 
-    const editTask = id => {
+    const toggleEditing = id => {
         setTaskList(
             taskList.map(task => task.id === id ? { ...task, isEditing: !task.isEditing } : task)
         );
