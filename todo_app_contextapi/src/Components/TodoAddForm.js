0ff7b08d74a0f51ad310961d289c3e262fe39dba
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-09-08 23:56:23 +0300

    Update TodoAddForm.js

--- todo_app_contextapi/src/Components/TodoAddForm.js
+++ todo_app_contextapi/src/Components/TodoAddForm.js
@@ -7,7 +7,9 @@ const TodoAddForm = () => {
     const [desc, setDesk] = useState(""); //Başlangıç değeri boş çünkü yeni görev girilen yer.
     const handleSubmit = (e) => {
         e.preventDefault();
-        context.addTask
+        context.addTask(desc);
+        setDesk(""); //içi temizleme
+        e.target.firstChild.firstChild.focus();
 
     }
     return (
@@ -17,7 +19,7 @@ const TodoAddForm = () => {
             {/* form butonuna tıklandığında handleSubmit fonks. çalıştır. */}
             <form onSubmit={handleSubmit}>
                 <div className="input-group mb-3">
-                    <input onChange={(e) => { setDesk(e.target.value) }} type="text" className="form-control" placeholder="Görev yaz" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <input onChange={(e) => { setDesk(e.target.value) }} value={desc} type="text" className="form-control" placeholder="Görev yaz" aria-label="Recipient's username" aria-describedby="button-addon2" />
                     <button className="btn btn-outline-success" type="submit" id="btn-add-task">Task Ekle</button>
                 </div>
             </form>
