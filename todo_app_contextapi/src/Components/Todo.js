de54ccb6e74f1a5156c85a6c66d498c70c1e4b1a
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-09-20 23:01:45 +0300

    Update Todo.js

--- todo_app_contextapi/src/Components/Todo.js
+++ todo_app_contextapi/src/Components/Todo.js
@@ -3,7 +3,17 @@ import React from 'react'
 const Todo = ({ task }) => {
     return (
         <div>
-            <li className={task.isCompleted ? "list-group-item rounded-2 mb-1 text-decoration-line-through disabled" : "list-group-item rounded-2 mb-1"}>{task.desc}</li>
+            <li className={task.isCompleted ? "list-group-item rounded-2 mb-1 text-decoration-line-through disabled" : "list-group-item rounded-2 mb-1"}>
+                <div>
+                    {task.desc}
+                </div>
+                <div>
+                    <button className='btn btn-warning btn-sm'>Düzenle</button>
+                    <button className='btn btn-danger btn-sm'>Sil</button>
+                </div>
+
+
+            </li>
         </div >
     )
 }
