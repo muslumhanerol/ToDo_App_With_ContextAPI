2b3a2477aa904622deab99fddbff5740afafb37a
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-09-20 23:07:31 +0300

    Update Todo.js

--- todo_app_contextapi/src/Components/Todo.js
+++ todo_app_contextapi/src/Components/Todo.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { TodoContext } from '../Contexts/TodoContext'
 
 const Todo = ({ task }) => {
+    const context = useContext(TodoContext);
     return (
         <div>
             <li className={task.isCompleted ? "d-flex justify-content-between list-group-item rounded-2 mb-1 text-decoration-line-through disabled" : "d-flex justify-content-between list-group-item rounded-2 mb-1"}>
@@ -9,7 +11,7 @@ const Todo = ({ task }) => {
                 </div>
                 <div>
                     <button className='btn btn-warning btn-sm me-1'>Düzenle</button>
-                    <button className='btn btn-danger btn-sm'>Sil</button>
+                    <button className='btn btn-danger btn-sm' onClick={() => { context.deleteTask(task.id) }}>Sil</button>
                 </div>
 
 
