71dc1b20ef038c9a70f49e80a327de052cb462af
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-09-24 23:38:48 +0300

    todo_editing

---
+++ todo_app_contextapi/src/Components/TodoEditForm.js
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from 'react';
+import { TodoContext } from '../Contexts/TodoContext';
+
+const TodoEditForm = ({ task }) => {
+    const context = useContext(TodoContext);
+    const [desc, setDesk] = useState(task.desc);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (desc) {
+            context.addTask(desc);
+            setDesk(""); //içi temizleme
+            e.target.firstChild.firstChild.focus();
+        } else {
+            alert("Lütfen Boş bırakmayınız.");
+        }
+    }
+    return (
+
+        <div className='container-fluid g-0'>
+            <h1 className='display-5 text-center mb-5 text-primary'>Todo App</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="input-group mb-3">
+                    <input onChange={(e) => { setDesk(e.target.value) }} value={desc} type="text" className="form-control" placeholder="Görev yaz" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <button className="btn btn-outline-success" type="submit" id="btn-add-task">Task Ekle</button>
+                </div>
+            </form>
+        </div>
+
+    )
+}
+
+export default TodoAddForm
