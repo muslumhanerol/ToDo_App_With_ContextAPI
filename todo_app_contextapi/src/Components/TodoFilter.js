01c51b886abfaf753283baf789f988c34f1ea832
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-10-02 23:24:44 +0300

    todo_filter_ok

--- todo_app_contextapi/src/Components/TodoFilter.js
+++ todo_app_contextapi/src/Components/TodoFilter.js
@@ -2,13 +2,16 @@ import React, { useContext } from 'react'
 import { TodoContext } from '../Contexts/TodoContext';
 
 const TodoFilter = () => {
-    const Context = useContext(TodoContext);
+    const context = useContext(TodoContext);
     return (
         <>
             <div className='container-fluid d-flex justify-content-between p-2 mb-1 g-0 border rounded-1'>
                 <div className='button-group'>
-                    <button></button>
+                    <button onClick={() => { context.setFilterMode("all"); console.log(context.filterMode) }} className='btn btn-success btn-sm me-1'>Hepsi</button>
 
+                    <button onClick={() => { context.setFilterMode(false); console.log(context.filterMode) }} className='btn btn-success btn-sm me-1'>Tamamlanmamış</button>
+
+                    <button onClick={() => { context.setFilterMode(true); console.log(context.filterMode) }} className='btn btn-success btn-sm me-1'>Tamamlandı</button>
                 </div>
                 <button className='btn btn-danger btn-sm' type='button' id='btn-clear-all' onClick={() => { useContext.clearAll() }}>Hepsini Temizle</button>
             </div>
