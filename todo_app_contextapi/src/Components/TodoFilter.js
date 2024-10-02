ba6da348e8ca5e11258cc7a9f4f6a97069e53b71
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-10-02 23:16:28 +0300

    Update TodoFilter.js

--- todo_app_contextapi/src/Components/TodoFilter.js
+++ todo_app_contextapi/src/Components/TodoFilter.js
@@ -4,9 +4,10 @@ const TodoFilter = () => {
     return (
         <>
             <div className='container-fluid d-flex justify-content-between p-2 mb-1 g-0 border rounded-1'>
-                <div>
+                <div className='button-group'>
 
                 </div>
+                <button className='btn btn-danger btn-sm' type='button' id='btn-clear-all' onClick={() => { useContext.clearAll() }}>Hepsini Temizle</button>
             </div>
         </>
     )
