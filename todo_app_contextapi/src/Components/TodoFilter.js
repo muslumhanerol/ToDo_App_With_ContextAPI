a3d4b8f27470d4a7f870694fbce83bdfcde4d62f
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-10-06 23:21:50 +0300

    Update TodoFilter.js

--- todo_app_contextapi/src/Components/TodoFilter.js
+++ todo_app_contextapi/src/Components/TodoFilter.js
@@ -7,13 +7,13 @@ const TodoFilter = () => {
         <>
             <div className='container-fluid d-flex justify-content-between p-2 mb-1 g-0 border rounded-1'>
                 <div className='button-group'>
-                    {/*Butona tıklandığında filtermodu all yapıyor*/}
+                    {/*Butona tıklandığında filtermodu all yapıyor. TodoWrapper.js 12satır.*/}
                     <button onClick={() => { context.setFilterMode("all"); console.log(context.filterMode) }} className='btn btn-success btn-sm me-1'>Hepsi</button>
 
-                    {/*Butona tıklandığında filtermodu all yapıyor*/}
+                    {/*Butona tıklandığında filtermodu false yapıyor.*/}
                     <button onClick={() => { context.setFilterMode(false); console.log(context.filterMode) }} className='btn btn-warning btn-sm me-1'>Tamamlanmamış</button>
 
-                    {/*Butona tıklandığında filtermodu all yapıyor*/}
+                    {/*Butona tıklandığında filtermodu true yapıyor.*/}
                     <button onClick={() => { context.setFilterMode(true); console.log(context.filterMode) }} className='btn btn-info btn-sm me-1'>Tamamlandı</button>
                 </div>
                 <button className='btn btn-danger btn-sm' type='button' id='btn-clear-all' onClick={() => { useContext.clearAll() }}>Hepsini Temizle</button>
