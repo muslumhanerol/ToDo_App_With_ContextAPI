0badda9ed9b522f69a3b3ae1b1b7373499c98128
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-10-15 23:22:42 +0300

    local_storage

--- todo_app_contextapi/src/Components/TodoWrapper.js
+++ todo_app_contextapi/src/Components/TodoWrapper.js
@@ -54,6 +54,10 @@ const TodoWrapper = ({ tasks }) => {
         setTaskList([]);
     }
 
+    const setLocalStorage = () => {
+
+    }
+
     //TodoContext içerisine yazılan herşeyde TodoContext de var olan propslar kullanılabilir olacak.
     return (
         //Göndermek istediklerimizi value içine yazılır. Ardından kullanmak isteiğimiz componentde import işlemi yapılır.
