0d57d5e89b521595e96014d1a40518f5d27a2ae4
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-10-24 23:56:00 +0300

    localestrage_ok

--- todo_app_contextapi/src/App.js
+++ todo_app_contextapi/src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 import TodoWrapper from './Components/TodoWrapper';
 import { v4 as idGenerate } from 'uuid';
 
-const INITIAL_STATE = [ //Proğramın başlandıcında atadığımız varsayılan değer.
-  { id: idGenerate(), desc: "Toplantı notlarını unutma", isCompleted: false, isEditing: false },
-  { id: idGenerate(), desc: "Müşterileri aramayı unutma", isCompleted: false, isEditing: false },
-  { id: idGenerate(), desc: "Spor salonundan randevu iptali", isCompleted: true, isEditing: false },
-  { id: idGenerate(), desc: "Proje yönetimi görüşmesi", isCompleted: false, isEditing: false },
-  { id: idGenerate(), desc: "Javascript konferansını unutma", isCompleted: true, isEditing: false }
-];
+// const INITIAL_STATE = [ //Proğramın başlandıcında atadığımız varsayılan değer.
+//   { id: idGenerate(), desc: "Toplantı notlarını unutma", isCompleted: false, isEditing: false },
+//   { id: idGenerate(), desc: "Müşterileri aramayı unutma", isCompleted: false, isEditing: false },
+//   { id: idGenerate(), desc: "Spor salonundan randevu iptali", isCompleted: true, isEditing: false },
+//   { id: idGenerate(), desc: "Proje yönetimi görüşmesi", isCompleted: false, isEditing: false },
+//   { id: idGenerate(), desc: "Javascript konferansını unutma", isCompleted: true, isEditing: false }
+// ];
 
 function App() {
 
   return (
     <>
-      <TodoWrapper tasks={INITIAL_STATE} />
+      <TodoWrapper />
     </>
   );
 }
